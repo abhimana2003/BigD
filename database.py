6d831a59eff211c84d5f5e3ada5bd54b792889b3
dtaimur <113263250+dtaimur@users.noreply.github.com>
2026-02-05 18:56:51 -0500

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql+psycopg2://danitaimur@localhost:5432/nutrition_ai"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base = declarative_base()
